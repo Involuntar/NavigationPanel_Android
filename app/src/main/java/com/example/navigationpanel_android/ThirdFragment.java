dd683c060ca90ab0f5227863f1232de7703ec4bc
Involuntar <ilya.nevolin.2004@mail.ru>
2025-10-27 20:51:16 +0500

    сделал отправку пуша на третьем фрагменте

--- app/src/main/java/com/example/navigationpanel_android/ThirdFragment.java
+++ app/src/main/java/com/example/navigationpanel_android/ThirdFragment.java
@@ -1,19 +1,37 @@
 package com.example.navigationpanel_android;
 
+import android.app.NotificationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ThirdFragment extends Fragment {
+import java.util.Objects;
 
+public class ThirdFragment extends Fragment {
+    ImageButton pushButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
+        View view = inflater.inflate(R.layout.fragment_third, container, false);
+
+        MyPushNotification myPushNotification = new MyPushNotification(getActivity(),
+                requireActivity().getSystemService(NotificationManager.class));
+
+        pushButton = view.findViewById(R.id.PushButton);
+        pushButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myPushNotification.sendNotify("Информация о студенте","ФИО: Неволин Илья Александрович\nГруппа: Т-423901-НТ ИСиТ");
+            }
+        });
+
+        return view;
     }
 }
